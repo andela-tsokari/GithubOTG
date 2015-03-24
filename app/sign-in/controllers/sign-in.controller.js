2a40507383408fd52ea57d3decede114aa3fb371
Tams Sokari <tams.sokari@andela.co>
2015-03-24 16:32:32 +0100

    fixing files

---
+++ app/sign-in/controllers/sign-in.controller.js
@@ -0,0 +1,14 @@
+(function () {
+  angular.module("GithubOTG")
+    .controller("Sign-InController", function () {
+
+      /*this.addUser = function (username) {
+        this.user = angular.copy(username);
+        this.username = "";
+      };*/
+
+      this.signIn = "https://github.com/login/oauth/authorize";
+
+    });
+    
+})();
